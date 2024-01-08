36f359aab1cfde4ce7b901a42459cd93a86d305d
William Lau <fireboxpig@gmail.com>
2024-01-07 16:21:27 -0800

    Prototyped Tab System

---
+++ UI Demo/Assets/Demo/TabUIHandler.cs
@@ -0,0 +1,61 @@
+/// 
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Demo
+{
+	public class TabUIHandler : MonoBehaviour
+	{
+		public Button generalPageButton;
+		public Button displayButton;
+		public Button audioPageButton;
+		public GameObject generalPageGameObject;
+		public GameObject displayPageGameObject;
+		public GameObject audioPageGameObject;
+
+		private void Awake()
+		{
+			Link();
+		}
+
+		public void Link()
+		{
+			generalPageButton.onClick.AddListener(HandleOnGeneralClick);
+			displayButton.onClick.AddListener(HandleOnDisplayClick);
+			audioPageButton.onClick.AddListener(HandleOnAudioClick);
+		}
+
+		public void Unlink()
+		{
+			generalPageButton.onClick.RemoveListener(HandleOnGeneralClick);
+			displayButton.onClick.RemoveListener(HandleOnDisplayClick);
+			audioPageButton.onClick.RemoveListener(HandleOnAudioClick);
+		}
+
+		private void HandleOnGeneralClick()
+		{
+			DisableAllPages();
+			generalPageGameObject.SetActive(true);
+		}
+
+		private void HandleOnDisplayClick()
+		{
+			DisableAllPages();
+			displayPageGameObject.SetActive(true);
+		}
+
+		private void HandleOnAudioClick()
+		{
+			DisableAllPages();
+			audioPageGameObject.SetActive(true);
+		}
+
+		private void DisableAllPages()
+		{
+			generalPageGameObject.SetActive(false);
+			displayPageGameObject.SetActive(false);
+			audioPageGameObject.SetActive(false);
+		}
+	}
+}
