a1c799f634b0a51d22f4cfe1ad779ea426fcdd0b
William Lau <fireboxpig@gmail.com>
2024-02-14 16:25:05 -0800

    Update AudioUtility.cs
    Added Clamp feature

--- UI Demo/Assets/Demo/AudioUtility.cs
+++ UI Demo/Assets/Demo/AudioUtility.cs
@@ -6,9 +6,17 @@ namespace Demo
 {
 	public static class AudioUtility
 	{
+		public static readonly float MIN_VOLUME = 0.00001f;
+		public static readonly float MAX_VOLUME = 1f;
+
 		public static float ConvertPercentToDecibels(float value)
 		{
 			return Mathf.Log10(value) * 20f;
+		}
+
+		public static float ClampVolumePercent(float value)
+		{
+			return Mathf.Clamp(value, MIN_VOLUME, MAX_VOLUME);
 		}
 	}
 }
