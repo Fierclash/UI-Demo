3f37248c8d1ce3ada69359418812d677505c3243
William Lau <fireboxpig@gmail.com>
2024-02-14 16:28:49 -0800

    Update SettingsPortLogic.cs
    Added Editor Utility

--- UI Demo/Assets/Demo/SettingsPortLogic.cs
+++ UI Demo/Assets/Demo/SettingsPortLogic.cs
@@ -3,6 +3,10 @@
 using System.IO;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace Demo
 {
 	public static class SettingsPortLogic
@@ -52,6 +56,10 @@ namespace Demo
 				writer.Write(json);
 				writer.Close();
 			}
+
+#if UNITY_EDITOR
+			AssetDatabase.Refresh();
+#endif
 		}
 	}
 }
