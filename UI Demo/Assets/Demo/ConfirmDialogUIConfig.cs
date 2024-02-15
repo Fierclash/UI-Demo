cc7c878022e82e39caeb9074e7c45ec1af75bb8d
William Lau <fireboxpig@gmail.com>
2024-02-14 16:25:21 -0800

    Created ConfirmDialogUIConfig

---
+++ UI Demo/Assets/Demo/ConfirmDialogUIConfig.cs
@@ -0,0 +1,11 @@
+/// #DataStructScript
+
+namespace Demo
+{
+	public struct ConfirmDialogUIConfig
+	{
+		public string messageLabel;
+		public string acceptLabel;
+		public string declineLabel;
+	}
+}
